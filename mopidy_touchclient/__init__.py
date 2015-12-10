1342e4e5625ed359345704fdc4f0a5914813d442
Dallas Brown <dbrown@hashbangind.com>
2015-12-09 22:18:19 -0700

    loading default config values on load

--- mopidy_touchclient/__init__.py
+++ mopidy_touchclient/__init__.py
@@ -30,6 +30,20 @@ class Extension(ext.Extension):
         schema = super(Extension, self).get_config_schema()
         schema['screen_width'] = config.Integer(minimum=1)
         schema['screen_height'] = config.Integer(minimum=1)
+        schema['cursor'] = config.Boolean
+        schema['cache_dir'] = config.String
+        schema['gpio'] = config.Boolean
+        schema['gpio_left'] = config.Integer(minimum=1)
+        schema['gpio_right'] = config.Integer(minimum=1)
+        schema['gpio_up'] = config.Integer(minimum=1)
+        schema['gpio_down'] = config.Integer(minimum=1)
+        schema['gpio_enter'] = config.Integer(minimum=1)
+        schema['sdl_fbdev'] = config.String
+        schema['sdl_mousdrv'] = config.String
+        schema['sdl_mousedev'] = config.String
+        schema['sdl_audiodriver'] = config.String
+        schema['sdl_path_dsp'] = config.String
+
         return schema
 
     def setup(self, registry):
