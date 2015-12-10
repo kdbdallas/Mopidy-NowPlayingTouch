95fd3b345c38089ada5c076cb2c1b618dcd5e046
Dallas Brown <dbrown@hashbangind.com>
2015-12-09 23:08:53 -0700

    config defaults updates

--- mopidy_touchclient/__init__.py
+++ mopidy_touchclient/__init__.py
@@ -4,11 +4,6 @@ from __future__ import unicode_literals
 
 import os
 
-# TODO: Remove entirely if you don't register GStreamer elements below
-#import pygst
-#pygst.require('0.10')
-#import gst
-#import gobject
 
 from mopidy import config, ext
 from .touchClientFrontend import TouchClient
@@ -30,19 +25,20 @@ class Extension(ext.Extension):
         schema = super(Extension, self).get_config_schema()
         schema['screen_width'] = config.Integer(minimum=1)
         schema['screen_height'] = config.Integer(minimum=1)
-        schema['cursor'] = config.Boolean
-        schema['cache_dir'] = config.String
-        schema['gpio'] = config.Boolean
-        schema['gpio_left'] = config.Integer(minimum=1)
-        schema['gpio_right'] = config.Integer(minimum=1)
-        schema['gpio_up'] = config.Integer(minimum=1)
-        schema['gpio_down'] = config.Integer(minimum=1)
-        schema['gpio_enter'] = config.Integer(minimum=1)
-        schema['sdl_fbdev'] = config.String
-        schema['sdl_mousdrv'] = config.String
-        schema['sdl_mousedev'] = config.String
-        schema['sdl_audiodriver'] = config.String
-        schema['sdl_path_dsp'] = config.String
+        schema['resolution_factor'] = config.Integer(minimum=6)
+        schema['cursor'] = config.Boolean()
+        schema['cache_dir'] = config.Path()
+        schema['gpio'] = config.Boolean()
+        schema['gpio_left'] = config.Integer()
+        schema['gpio_right'] = config.Integer()
+        schema['gpio_up'] = config.Integer()
+        schema['gpio_down'] = config.Integer()
+        schema['gpio_enter'] = config.Integer()
+        schema['sdl_fbdev'] = config.String()
+        schema['sdl_mousdrv'] = config.String()
+        schema['sdl_mousedev'] = config.String()
+        schema['sdl_audiodriver'] = config.String()
+        schema['sdl_path_dsp'] = config.String()
 
         return schema
 
